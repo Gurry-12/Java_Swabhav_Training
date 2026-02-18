76734d7967abff0d3ca05791c2b19346a18be6a3
Gurry-12 <96852785+Gurry-12@users.noreply.github.com>
2026-02-18 10:12:13 +0530

    Initial commit

---
+++ Day-1-Introduction/src/intoduction_to_java/Main.java
@@ -0,0 +1,11 @@
+package intoduction_to_java;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+
+	}
+
+}
---
+++ Day-2-OPPS/src/com/swabhav/encapsulation/models/BankAccount.java
@@ -0,0 +1,5 @@
+package com.swabhav.encapsulation.models;
+
+public class BankAccount {
+
+}
